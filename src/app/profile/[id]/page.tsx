21245d60d0e1dcec5fa23f0ef8f7d34ec8cf1c32
Ryan Gertz <114454968+RyanGertz@users.noreply.github.com>
2024-10-30 03:31:02 +0000

    chakra

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,8 +1,17 @@
+import { Avatar} from '@/components/ui/avatar'
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const id = (await params).id;
-  return <div>My Post: {id}</div>;
+  return (
+    <div>
+      <Avatar>
+
+      </Avatar>
+      {id}
+    </div>
+  );
 }
