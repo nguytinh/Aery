a473bfe358fba865500a4e60f27bf059728f5e8e
Ryan Gertz <114454968+RyanGertz@users.noreply.github.com>
2024-11-15 04:20:23 +0000

    api

--- src/app/createpost/page.tsx
+++ src/app/createpost/page.tsx
@@ -5,11 +5,11 @@ import { useForm } from "react-hook-form";
 import Navbar from '../../components/navbar';
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { Box, useColorModeValue, Flex, Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+import { Box, Flex, Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
 
 
 const postSchema = z.object({
-    post: z.string().email("Invalid email address"),
+    postName: z.string(),
     description: z.string(),
     imageUrl: z.string()
 });
@@ -21,7 +21,22 @@ const CreatePost: React.FC = () => {
 
     
 
-    const { postInfo, handleSubmit, formState: { errors } } = useForm<postFormData>({resolver: zodResolver(postSchema),});
+    const { register, handleSubmit, formState: { errors } } = useForm<postFormData>({resolver: zodResolver(postSchema)});
+
+    const handleCreatePost = async (data: postFormData) => {
+        // debugger
+        const response = await fetch("/api/posts/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        if (!response.ok) {
+            console.error("Failed to create post:", response);
+            return;
+        }
+    }
 
     // if (status != "authenticated") {
     //     return (
@@ -43,19 +58,29 @@ const CreatePost: React.FC = () => {
                 
                 <Box bg="white" p={8} rounded="md" shadow="lg" width="full" maxW="md">
                 <Heading as="h1" size="lg" textAlign="center" mb={6}>Create Your Post Below:</Heading>
-                <form>
+                <form onSubmit={handleSubmit(handleCreatePost)}>
                     <FormControl>
                         <FormLabel>Post Name</FormLabel>
-                        <Input></Input>
+                        <Input type='text'
+                        {...register("postName")} 
+                        />
                     </FormControl>
+
                     <FormControl>
                         <FormLabel>Post Description</FormLabel>
-                        <Input></Input>
+                        <Input type='text'
+                        {...register("description")}
+                        />
                     </FormControl>
+
                     <FormControl>
                         <FormLabel>Image URL</FormLabel>
-                        <Input placeholder="https://imageurl.domain"></Input>
+                        <Input {...register("imageUrl")} 
+                        placeholder="https://imageurl.domain" 
+                        type='text' 
+                        />
                     </FormControl>
+
                     <Button
                         type="submit"
                         colorScheme="blue"
